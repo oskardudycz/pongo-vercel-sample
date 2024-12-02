3cc2adf23426cfabee54854126f6e233dd278e23
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-12-02 18:06:56 +0100

    Removed nesting

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -2,6 +2,7 @@ export const dynamic = 'force-dynamic'; // static by default, unless reading the
 
 export const runtime = 'nodejs';
  
-export function GET() {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function GET(request: Request) {
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
