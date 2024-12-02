9dca1174c4e24110586ad1742482b91dfe53213a
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-12-02 17:45:10 +0100

    added runtime explicitly

--- src/api/hello/route.ts
+++ src/api/hello/route.ts
@@ -1,4 +1,6 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+
+export const runtime = 'nodejs';
  
 export function GET() {
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
