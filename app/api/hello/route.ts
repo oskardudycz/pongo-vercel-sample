1f14378aeff209295fd1c98850826016bca1dd8c
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-12-03 08:32:58 +0100

    Left only vanilla vercel function

--- app/api/hello/route.ts
+++
@@ -1,8 +0,0 @@
-export const dynamic = 'force-dynamic'; // static by default, unless reading the request
-
-export const runtime = 'nodejs';
- 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function GET(request: Request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
-}
