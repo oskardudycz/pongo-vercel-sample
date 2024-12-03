62ea205d44ca13e1f9cf4ef4cdec086fe92824a1
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-12-03 09:47:44 +0100

    Added Pongo

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,10 @@
+import { pongoClient } from '@event-driven-io/pongo';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const pongo = pongoClient(process.env.DATABASE_URL, { connectionOptions: { pooled:false } });
+
   res.status(200).json({
     message: 'Hello world!',
     cookies: req.cookies,
