c658c495303552419636582e5adc4bbd6303c7c3
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-12-03 09:53:47 +0100

    Added test query and set project to module

--- api/hello.ts
+++ api/hello.ts
@@ -1,13 +1,21 @@
 import { pongoClient } from '@event-driven-io/pongo';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(_req: VercelRequest, res: VercelResponse) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const pongo = pongoClient(process.env.DATABASE_URL, { connectionOptions: { pooled:false } });
 
-  res.status(200).json({
-    message: 'Hello world!',
-    cookies: req.cookies,
-    envVar: process.env.API_KEY,
-  });
+  const users = pongo.db().collection("cloudflare");
+
+  const now = new Date().toISOString();
+
+  const {insertedId } = await users.insertOne({_id: now, name: `test-${now}`});
+
+  // Test query
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const {_version, ...user } = await users.findOne({_id: insertedId!});
+
+  console.log(JSON.stringify(user));
+
+  res.status(200).json({ user });
 }
