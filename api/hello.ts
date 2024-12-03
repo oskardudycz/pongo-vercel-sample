2386f088bfaf13511912c4d670ccd8b6e548baad
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-12-03 14:56:54 +0100

    Commented out Pongo

--- api/hello.ts
+++ api/hello.ts
@@ -1,23 +1,29 @@
-import { pongoClient } from '@event-driven-io/pongo';
+//import { pongoClient } from '@event-driven-io/pongo';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export const runtime = 'nodejs';
 
-export default async function handler(_req: VercelRequest, res: VercelResponse) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const pongo = pongoClient(process.env.DATABASE_URL, { connectionOptions: { pooled:false } });
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   const pongo = pongoClient(process.env.DATABASE_URL, { connectionOptions: { pooled:false } });
 
-  const users = pongo.db().collection("cloudflare");
+//   const users = pongo.db().collection("cloudflare");
 
-  const now = new Date().toISOString();
+//   const now = new Date().toISOString();
 
-  const {insertedId } = await users.insertOne({_id: now, name: `test-${now}`});
+//   const {insertedId } = await users.insertOne({_id: now, name: `test-${now}`});
 
-  // Test query
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const {_version, ...user } = await users.findOne({_id: insertedId!});
+//   // Test query
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   const {_version, ...user } = await users.findOne({_id: insertedId!});
 
-  console.log(JSON.stringify(user));
+//   console.log(JSON.stringify(user));
 
-  res.status(200).json({ user });
+//   res.status(200).json({ user });
+
+  res.status(200).json({
+    message: 'Hello world!',
+    cookies: req.cookies,
+    envVar: process.env.API_KEY,
+  });
 }
